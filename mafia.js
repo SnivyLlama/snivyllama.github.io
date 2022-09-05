79059ef810e78a7f3c62a1ec7e04a6b9248e633a
SnivyLlama <raoabi27@gmail.com>
2022-09-04 21:37:05 -0500

    Made JS a bit more readable

--- mafia.js
+++ mafia.js
@@ -1,11 +1,11 @@
 fetch("https://api.github.com/gists/e57ffb6b7389047c6d609dcad4ed5a66")
 .then(resp => resp.json())
 .then(data => {
-    rooms = JSON.parse(data.files["mafia.json"].content).rooms
-    params = new URL(window.location.href).searchParams
-    user = params.get("user")
-    redirect = params.get("r")
-    text = document.getElementById("text")
+    let rooms = JSON.parse(data.files["mafia.json"].content).rooms
+    let params = new URL(window.location.href).searchParams
+    let user = params.get("user")
+    let redirect = params.get("r")
+    let text = document.getElementById("text")
     for (const room of rooms) {
         if (room.hostUser.username == user || !user) {
             if (text.innerHTML == "Loading...") {
