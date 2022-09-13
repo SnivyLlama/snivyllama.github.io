e4ae48cc58ead7eef18b36ff42b14493aea03065
SnivyLlama <raoabi27@gmail.com>
2022-09-13 14:59:17 -0500

    Big changes, these are untested however

--- school.js
+++ school.js
@@ -1,5 +1,7 @@
-var progress = document.getElementById("progress")
-var bar = document.getElementById("bar")
+var progress = document.getElementById("progress");
+var specific = document.getElementById("specific");
+var obar = document.getElementById("overall-bar");
+var sbar = document.getElementById("specific-bar");
 
 function minutes(date) {
     return date.getUTCHours() * 60 + date.getUTCMinutes();
@@ -13,13 +15,52 @@ function update() {
     let now = new Date();
     if (now.getUTCDay() % 6 == 0 || minutes(now) < 800 || minutes(now) >= 1200) {
         progress.innerHTML = "School is not in session at this time!";
-        bar.setAttribute("width", "99.8%");
+        specific.innerHTML  = "School is not in session at this time!";
+        obar.setAttribute("width", "99.8%");
+        sbar.setAttribute("width", "99.8%");
     } else {
         let percent = (acc(now) - 800) / 4;
-        let mins = 400 - (minutes(now) - 800);
+        let amins = minutes(now) - 800
+        let mins = 400 - amins;
         progress.innerHTML = `School is ${percent.toFixed(4)}% done! Only ${Math.floor(mins/60) > 0 ? Math.floor(mins/60) + (Math.floor(mins/60) == 1 ? " hour" : " hours") : ""}${Math.floor(mins/60) > 0 && mins % 60 > 0 ? " and " : ""}${mins % 60 > 0 ? mins % 60 + (mins % 60 == 1 ? " minute left" : " minutes left") : ""}!`;
-        bar.setAttribute("width", (percent * 0.998) + "%");
+        obar.setAttribute("width", (percent * 0.998) + "%");
+        if (amins < 86) {
+            percent = (acc(now) - 800) / .86;
+            mins = 86 - amins;
+            specific.innerHTML = `Block 1 is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 86 && amins < 94) {
+            percent = (acc(now) - 886) / .08;
+            mins = 94 - amins;
+            specific.innerHTML = `Passing Time is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 94 && amins < 180) {
+            percent = (acc(now) - 894) / .86;
+            mins = 180 - amins;
+            specific.innerHTML = `Block 2 is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 180 && amins < 188) {
+            percent = (acc(now) - 980) / .08;
+            mins = 188 - amins;
+            specific.innerHTML = `Passing Time is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 188 && amins < 306) {
+            percent = (acc(now) - 988) / 1.18;
+            mins = 306 - amins;
+            specific.innerHTML = `Block 3 is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 306 && amins < 314) {
+            percent = (acc(now) - 1106) / .08;
+            mins = 314 - amins;
+            specific.innerHTML = `Passing Time is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        } else if (amins >= 314) {
+            percent = (acc(now) - 1114) / .86;
+            mins = 400 - amins;
+            specific.innerHTML = `Block 4 is ${percent.toFixed(4)}% done! Only ${mins + (mins == 1 ? " minute" : " minutes")} left!`;
+            sbar.setAttribute("width", (percent * 0.998) + "%");
+        }
     }
 }
 
-setInterval(update, 50)
+setInterval(update, 50);
