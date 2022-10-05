0e0d2b74b9b2302012ea165f261cea389fcae2eb
SnivyLlama <raoabi27@gmail.com>
2022-10-04 21:16:46 -0500

    Remove quadratic thing

--- index.js
+++ index.js
@@ -15,36 +15,19 @@ const lifespan = 2486372248080;
 function update() {
     if (date.value == "") {
         bar.setAttribute("width", "99.8%");
-        text.innerHTML = "nonexistant!";
+        text.innerHTML = "nonexistent!";
     } else {
         let ts = new Date(date.value).getTime();
-    let percent = (Date.now() - ts) / lifespan * 100;
-    if (percent < 0) {
-        bar.setAttribute("width", "99.8%");
-        text.innerHTML = "nonexistant!";
-    } else if (percent > 100) {
-        bar.setAttribute("width", "99.8%");
-        text.innerHTML = "nonexistant! (or just very lucky!)";
-    } else {
-        text.innerHTML = `${percent.toFixed(8)}% done!`;
-        bar.setAttribute("width", (percent * 0.998) + "%");
-    }
-    }
-    if (a.value && b.value && c.value) {
-        let sqt = b.value ** 2 + (-4 * a.value * c.value);
-        if (sqt < 0) {
-            quad.innerHTML = "Non-real answer!";
-            return;
-        } else if (a.value == 0) {
-            quad.innerHTML = "A cannot equal 0!";
-            return;
-        }
-        let anspos = (-b.value + Math.sqrt(sqt)) / (2 * a.value);
-        let ansneg = (-b.value - Math.sqrt(sqt)) / (2 * a.value);
-        if (anspos == ansneg) {
-            quad.innerHTML = `A zero of ${anspos} was found!`;
+        let percent = (Date.now() - ts) / lifespan * 100;
+        if (percent < 0) {
+            bar.setAttribute("width", "99.8%");
+            text.innerHTML = "nonexistent!";
+        } else if (percent > 100) {
+            bar.setAttribute("width", "99.8%");
+            text.innerHTML = "nonexistent! (or just very lucky!)";
         } else {
-            quad.innerHTML = `Two zeros of ${anspos} and ${ansneg} were found!`;
+            text.innerHTML = `${percent.toFixed(8)}% done!`;
+            bar.setAttribute("width", (percent * 0.998) + "%");
         }
     }
 }
