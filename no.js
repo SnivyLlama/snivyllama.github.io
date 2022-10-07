4b6020790ab5eff2d891d5ac465ff8b71c55812f
SnivyLlama <raoabi27@gmail.com>
2022-10-06 20:21:36 -0500

    Crash site

---
+++ no.js
@@ -0,0 +1,6 @@
+var suck = document.getElementById("reasons-why-u-suck");
+while (true) {
+    let li = document.createElement("li");
+    li.innerText = "You suck bro!";
+    suck.appendChild(li);
+}
