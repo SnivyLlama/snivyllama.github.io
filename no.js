76b7f6cd2dc69f5a9fa4bf3fd74696b9215a6c69
SnivyLlama <raoabi27@gmail.com>
2022-10-09 15:48:04 -0500

    Delete unneeded pages

--- no.js
+++
@@ -1,6 +0,0 @@
-var suck = document.getElementById("reasons-why-u-suck");
-while (true) {
-    let li = document.createElement("li");
-    li.innerText = "You suck bro!";
-    suck.appendChild(li);
-}
