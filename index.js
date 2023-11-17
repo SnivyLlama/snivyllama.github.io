3f718f1f46d134da4bb154d0127f102825a877cb
SnivyLlama <61280616+SnivyLlama@users.noreply.github.com>
2023-11-17 11:48:40 -0600

    upd

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var bar = document.getElementById("whyrureadingthis");
-var text = document.getElementById("whyrureadingthistext");
-var date = document.getElementById("whyureadingthisdate");
 var a = document.getElementById("a");
 var b = document.getElementById("b");
 var c = document.getElementById("c");
@@ -17,23 +15,6 @@ const sunborn = -145065600000000000000;
 const sundie = 157680000000000000000;
 
 function update() {
-    if (date.value == "") {
-        bar.setAttribute("width", "99.8%");
-        text.innerHTML = "nonexistent!";
-    } else {
-        let ts = new Date(date.value).getTime();
-        let percent = (Date.now() - ts) / lifespan * 100;
-        if (percent < 0) {
-            bar.setAttribute("width", "99.8%");
-            text.innerHTML = "nonexistent!";
-        } else if (percent > 100) {
-            bar.setAttribute("width", "99.8%");
-            text.innerHTML = "nonexistent! (or just very lucky!)";
-        } else {
-            text.innerHTML = `${percent.toFixed(8)}% done!`;
-            bar.setAttribute("width", (percent * 0.998) + "%");
-        }
-    }
     let percent = (Date.now() - sunborn) / (sundie - sunborn) * 100;
     suntext.innerHTML = `The sun is ${percent.toFixed(14)}% of the way to going kaboom!`;
     sun.setAttribute("width", (percent * 0.998) + "%");
